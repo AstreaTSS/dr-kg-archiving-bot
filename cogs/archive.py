36e297201fe0988e4664ca14c9d9ce63a540f520
Sonic4999 <sonictbm@gmail.com>
2021-11-28 21:59:52 -0500

    Initial push
    Everything should be added, though nothing has been tested

---
+++ cogs/archive.py
@@ -0,0 +1,206 @@
+import os
+import subprocess
+import typing
+import urllib.parse
+from pathlib import Path
+
+import attr
+import discord
+import rtoml
+from discord.ext import commands
+
+config = rtoml.load(Path(os.environ.get("CONFIG_PATH")))
+
+os.environ.get("DIRECTORY_OF_FILE")
+
+
+@attr.s
+class BaseChannel:
+    id: int = attr.ib()
+    name: str = attr.ib()
+
+    @property
+    def path(self) -> str:
+        return config["archive_location"]
+
+    @property
+    def base_url(self) -> str:
+        return os.environ.get("WEBSITE_BASE") + config["github_name"]
+
+
+@attr.s(slots=True)
+class Category(BaseChannel):
+    internal_name: str = attr.ib()
+    channels: typing.List["Channel"] = attr.ib(factory=list)
+
+    @property
+    def path(self):
+        return f"{super().path}/{self.internal_name}"
+
+    @property
+    def url_quote(self):
+        return urllib.parse.quote(f"{self.internal_name}/{self.internal_name}.md")
+
+    @property
+    def url_path(self):
+        return f"{self.base_url}/{self.url_quote}"
+
+    def mkdir(self):
+        os.mkdir(self.path)
+
+
+@attr.s(slots=True)
+class Channel(BaseChannel):
+    category: Category = attr.ib()
+    threads: typing.List["Thread"] = attr.ib(factory=list)
+
+    @property
+    def path(self):
+        return f"{super().path}/{self.category.internal_name}/{self.id}.html"
+
+    @property
+    def url_quote(self):
+        return urllib.parse.quote(f"{self.category.internal_name}/{self.id}.html")
+
+    @property
+    def url_path(self):
+        return f"{self.base_url}/{self.url_quote}"
+
+    @property
+    def folder_path(self):
+        return self.path.replace(".html", "")
+
+    @property
+    def proper_name(self):
+        return self.name.replace("-", " ").title()
+
+    def mkdir(self):
+        os.mkdir(self.folder_path)
+
+
+@attr.s(slots=True)
+class Thread(BaseChannel):
+    channel: Channel = attr.ib()
+
+    @property
+    def path(self):
+        return f"{super().path}/{self.channel.category.internal_name}/{self.channel.id}/{self.id}.html"
+
+    @property
+    def url_quote(self):
+        return urllib.parse.quote(
+            f"{self.channel.category.internal_name}/{self.channel.id}/{self.id}.html"
+        )
+
+    @property
+    def url_path(self):
+        return f"{self.base_url}/{self.url_quote}"
+
+
+class Archive(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+
+    async def unarchive_threads(self, ctx: commands.Context):
+        async with ctx.channel.typing():
+            for category_entry in config["categories"]:
+                category: discord.CategoryChannel = ctx.guild.get_channel(
+                    category_entry["id"]
+                )  # type: ignore
+                for channel in category.text_channels:
+                    async for archived_channel in channel.archived_threads(limit=None):
+                        await archived_channel.edit(archived=False)
+
+        await ctx.reply("Done!")
+
+    async def archive(self, ctx: commands.Context):
+        categories: typing.List[Category] = []
+
+        await ctx.reply("Here we go. This will take a *long* time.")
+
+        async with ctx.channel.typing():
+            for category_entry in config["categories"]:
+                category = Category(
+                    category_entry["id"],
+                    category_entry["name"],
+                    category_entry["internal_name"],
+                )
+                category.mkdir()
+
+                category_channel: discord.CategoryChannel = ctx.guild.get_channel(
+                    category.id
+                )  # type: ignore
+                for discord_channel in category_channel.text_channels:
+                    channel = Channel(
+                        discord_channel.id, discord_channel.name, category
+                    )
+
+                    if discord_channel.threads:
+                        channel.mkdir()
+
+                        for discord_thread in discord_channel.threads:
+                            thread = Thread(
+                                discord_thread.id, discord_thread.name, channel
+                            )
+                            channel.threads.append(thread)
+
+                        subprocess.run(
+                            [
+                                "dotnet",
+                                str(os.environ.get("EXPORTER_DLL_PATH")),
+                                "export -t",
+                                f'"{os.environ.get("MAIN_TOKEN")}"',
+                                "-b -c",
+                                " ".join([str(t.id) for t in channel.threads]),
+                                "-o",
+                                f"{channel.folder_path}/%c",
+                            ]
+                        )
+
+                    category.channels.append(channel)
+
+                subprocess.run(
+                    [
+                        "dotnet",
+                        str(os.environ.get("EXPORTER_DLL_PATH")),
+                        "export -t",
+                        f'"{os.environ.get("MAIN_TOKEN")}"',
+                        "-b -c",
+                        " ".join([str(c.id) for c in category.channels]),
+                        "-o",
+                        f"{category.path}/%c",
+                    ]
+                )
+
+                with open(
+                    f"{category.path}/{category.internal_name}.md",
+                    "w",
+                    encoding="utf-8",
+                ) as md_file:
+                    md_file.write(
+                        f"# Season {config['season_num']} - {category.name}\n\nAll Locations:\n"
+                    )
+                    for channel in category.channels:
+                        md_file.write(
+                            f"* [{channel.proper_name}]({channel.url_path})\n"
+                        )
+
+                        for thread in channel.threads:
+                            md_file.write(f"  * [{thread.name}]({thread.url_path})\n")
+                    md_file.write(f"\n[Back to Home]({channel.base_url})")
+
+                categories.append(category)
+
+            with open(
+                f"{config['archive_location']}/README.md", "w", encoding="utf-8"
+            ) as md_file:
+                md_file.write("# Home Page\n\nAll Categories:\n")
+
+                for category in categories:
+                    md_file.write(f"* [{category.name}]({category.url_path})\n")
+
+        await ctx.reply("Done!")
+
+
+def setup(bot):
+    bot.add_cog(Archive(bot))
---
+++ common/utils.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python3.8
+import collections
+import logging
+import traceback
+import typing
+from decimal import Decimal
+from decimal import InvalidOperation
+from pathlib import Path
+
+import aiohttp
+import discord
+from discord.ext import commands
+
+
+class DecimalConverter(commands.Converter):
+    async def convert(self, ctx: commands.Context, argument: str) -> Decimal:
+        try:
+            return Decimal(argument)
+        except InvalidOperation:
+            raise commands.BadArgument("This is not a decimal!")
+
+
+class CustomCheckFailure(commands.CheckFailure):
+    # custom classs for custom prerequisite failures outside of normal command checks
+    pass
+
+
+async def error_handle(bot, error, ctx: typing.Union[commands.Context, None] = None):
+    # handles errors and sends them to owner
+    if isinstance(error, aiohttp.ServerDisconnectedError):
+        to_send = "Disconnected from server!"
+        split = True
+    else:
+        error_str = error_format(error)
+        logging.getLogger("discord").error(error_str)
+
+        error_split = error_str.splitlines()
+        chunks = [error_split[x : x + 20] for x in range(0, len(error_split), 20)]
+        for chunk_ in chunks:
+            chunk_[0] = f"```py\n{chunk_[0]}"
+            chunk_[len(chunk_) - 1] += "\n```"
+
+        final_chunks = ["\n".join(chunk) for chunk in chunks]
+        if ctx and hasattr(ctx, "message") and hasattr(ctx.message, "jump_url"):
+            final_chunks.insert(0, f"Error on: {ctx.message.jump_url}")
+
+        to_send = final_chunks
+        split = False
+
+    await msg_to_owner(bot, to_send, split)
+
+    if ctx:
+        if isinstance(ctx, commands.Context):
+            await ctx.reply(
+                "An internal error has occured. The bot owner has been notified."
+            )
+        else:
+            try:
+                await ctx.create_response(
+                    content="An internal error has occured. The bot owner has been notified.",
+                    ephemeral=True,
+                )
+            except discord.NotFound:
+                try:
+                    await ctx.edit(
+                        content="An internal error has occured. The bot owner has been notified.",
+                    )
+                except discord.HTTPException:
+                    pass
+
+
+async def msg_to_owner(bot, content, split=True):
+    # sends a message to the owner
+    owner = bot.owner
+    string = str(content)
+
+    str_chunks = string_split(string) if split else content
+    for chunk in str_chunks:
+        await owner.send(f"{chunk}")
+
+
+def line_split(content: str, split_by=20):
+    content_split = content.splitlines()
+    return [
+        content_split[x : x + split_by] for x in range(0, len(content_split), split_by)
+    ]
+
+
+def embed_check(embed: discord.Embed) -> bool:
+    """Checks if an embed is valid, as per Discord's guidelines.
+    See https://discord.com/developers/docs/resources/channel#embed-limits for details."""
+    if len(embed) > 6000:
+        return False
+
+    if embed.title and len(embed.title) > 256:
+        return False
+    if embed.description and len(embed.description) > 2048:
+        return False
+    if embed.author and embed.author.name and len(embed.author.name) > 256:
+        return False
+    if embed.footer and embed.footer.text and len(embed.footer.text) > 2048:
+        return False
+    if embed.fields:
+        if len(embed.fields) > 25:
+            return False
+        for field in embed.fields:
+            if field.name and len(field.name) > 1024:
+                return False
+            if field.value and len(field.value) > 2048:
+                return False
+
+    return True
+
+
+def deny_mentions(user):
+    # generates an AllowedMentions object that only pings the user specified
+    return discord.AllowedMentions(everyone=False, users=[user], roles=False)
+
+
+def error_format(error):
+    # simple function that formats an exception
+    return "".join(
+        traceback.format_exception(
+            etype=type(error), value=error, tb=error.__traceback__
+        )
+    )
+
+
+def string_split(string):
+    # simple function that splits a string into 1950-character parts
+    return [string[i : i + 1950] for i in range(0, len(string), 1950)]
+
+
+def file_to_ext(str_path, base_path):
+    # changes a file to an import-like string
+    str_path = str_path.replace(base_path, "")
+    str_path = str_path.replace("/", ".")
+    return str_path.replace(".py", "")
+
+
+def get_all_extensions(str_path, folder="cogs"):
+    # gets all extensions in a folder
+    ext_files = collections.deque()
+    loc_split = str_path.split("cogs")
+    base_path = loc_split[0]
+
+    if base_path == str_path:
+        base_path = base_path.replace("main.py", "")
+    base_path = base_path.replace("\\", "/")
+
+    if base_path[-1] != "/":
+        base_path += "/"
+
+    pathlist = Path(f"{base_path}/{folder}").glob("**/*.py")
+    for path in pathlist:
+        str_path = str(path.as_posix())
+        str_path = file_to_ext(str_path, base_path)
+
+        if str_path != "cogs.db_handler":
+            ext_files.append(str_path)
+
+    return ext_files
+
+
+def toggle_friendly_str(bool_to_convert):
+    if bool_to_convert == True:
+        return "on"
+    else:
+        return "off"
+
+
+def yesno_friendly_str(bool_to_convert):
+    if bool_to_convert == True:
+        return "yes"
+    else:
+        return "no"
+
+
+def error_embed_generate(error_msg):
+    return discord.Embed(colour=discord.Colour.red(), description=error_msg)
+
+
+def proper_permissions():
+    async def predicate(ctx: commands.Context):
+        # checks if author has admin or manage guild perms or is the owner
+        permissions = ctx.channel.permissions_for(ctx.author)
+        return permissions.administrator or permissions.manage_guild
+
+    return commands.check(predicate)
+
+
+def deprecated_cmd():
+    async def predicate(ctx: commands.Context):
+        await ctx.reply(
+            embed=error_embed_generate(
+                "This command is deprecated, "
+                + "and will be removed in a later release. Please use "
+                + f"`/{ctx.command.qualified_name.replace('_', '-')}`` instead."
+            )
+        )
+        return True
+
+    return commands.check(predicate)
