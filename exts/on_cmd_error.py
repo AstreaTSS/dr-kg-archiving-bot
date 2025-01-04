1a3ebd647cd3f9827f9f354969b6ae5e24689f49
AstreaTSS <25420078+AstreaTSS@users.noreply.github.com>
2025-01-03 22:55:04 -0500

    fix: oops

--- exts/on_cmd_error.py
+++ exts/on_cmd_error.py
@@ -1,12 +1,3 @@
-"""
-Copyright 2021-2024 AstreaTSS.
-This file is part of PYTHIA.
-
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at https://mozilla.org/MPL/2.0/.
-"""
-
 import importlib
 
 import interactions as ipy
